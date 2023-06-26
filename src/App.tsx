2cbdcebac6e5022c1c1c924783d96d0908b4e0aa
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-06-26 15:47:47 +0500

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useDidUpdateEffect } from './utils/custom-effect';
+import { Routing } from './routing';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  useDidUpdateEffect(() => {
+    console.log('Count updated:', count);
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routing />  
+  )
+  
 }
 
 export default App;
